0a43e9620752e13f71b5f1553b5d550ea40ed441
Binh Truong <binhtt.13122000@gmail.com>
2022-02-21 22:58:39 +0700

    binhtt:update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,38 @@
 import { Module } from "@nestjs/common";
+import {
+  APP_FILTER,
+  APP_INTERCEPTOR,
+  RouterModule,
+  Routes,
+} from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { configService } from "./config/dbConfig.service";
-
+import { HttpExceptionFilter } from "./filters/http-exception.filter";
+import { ResponseDataInterceptor } from "./interceptors/response-data.interceptor";
+import { AuthModule } from "./modules/auth/auth.module";
+const routes: Routes = [
+  {
+    path: "auth",
+    module: AuthModule,
+  },
+];
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    RouterModule.register(routes),
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseDataInterceptor,
+    },
+  ],
 })
 export class AppModule {}
