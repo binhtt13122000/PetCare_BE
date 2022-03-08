ddf0fbf4260978f8fd5f20cd92e52bad938e2fbf
Binh Truong <binhtt.13122000@gmail.com>
2022-03-09 06:22:25 +0700

    binhtt: fix Update API and remove feature delete file temporarily

--- src/modules/pets/pets.controller.ts
+++ src/modules/pets/pets.controller.ts
@@ -58,13 +58,13 @@ export class PetsController {
       let avatar = null;
       if (file) {
         avatar = await uploadService.uploadFile(file);
-        await uploadService.removeImage(body.avatar);
+        // await uploadService.removeImage(body.avatar);
       }
       const pet: Partial<Pet> = {
         ...body,
         avatar: file ? avatar : body.avatar,
       };
-      return await this.petsService.store(pet);
+      return await this.petsService.update(pet.id, pet);
     } catch (error) {
       throw new HttpException(error, HttpStatus.BAD_REQUEST);
     }
