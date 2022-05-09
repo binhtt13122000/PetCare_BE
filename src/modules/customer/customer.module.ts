3f15a0dc9fd24f74350774acf9752c9bf3a9c7f9
nhanlt1602 <nhanlt16235@gmail.com>
2022-05-09 15:15:00 +0700

    [NhanLT/9-5] add ShareModule in CustomerModule

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,12 +1,17 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { SharedModule } from "src/shared/shared.module";
 import { UserModule } from "../users/user.module";
 import { CustomerController } from "./customer.controller";
 import { CustomerRepository } from "./customer.repository";
 import { CustomerService } from "./customer.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CustomerRepository]), UserModule],
+  imports: [
+    TypeOrmModule.forFeature([CustomerRepository]),
+    UserModule,
+    SharedModule,
+  ],
   controllers: [CustomerController],
   providers: [CustomerService],
   exports: [CustomerService],
