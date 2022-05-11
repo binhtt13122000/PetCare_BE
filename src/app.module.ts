8000cb86722bbb50bc704cfa953cca9216cbf14d
nhanlt1602 <nhanlt16235@gmail.com>
2022-05-11 14:26:40 +0700

    [NhanLT/11-5] CRUD service fee api

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,7 +27,7 @@ import { TransactionFeesModule } from "./modules/transaction-fees/transaction-fe
 import { ChatGateway } from "./modules/chat/chat.gateway";
 import { BreedsModule } from "./modules/breeds/breeds.module";
 import { SpeciesModule } from "./modules/species/species.module";
-import { ServiceFeesModule } from "./service-fees/service-fees.module";
+import { ServiceFeesModule } from "./modules/service-fees/service-fees.module";
 import { ServiceFeeService } from "./service-fee/service-fee.service";
 
 const mongoConnectionString = configService.getMongoConnectionString();
--- src/service-fees/service-fees.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller("service-fees")
-export class ServiceFeesController {}
