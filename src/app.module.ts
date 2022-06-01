58d0f7c0e72200a6c6315543e90131b745e27aa2
Binh Truong <binhtt.13122000@gmail.com>
2022-06-01 09:49:26 +0700

    binhtt: hot fix chat gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,6 +35,7 @@ import { FollowsModule } from "./modules/follows/follows.module";
 import { ReportsModule } from "./modules/reports/reports.module";
 import { TicketsModule } from "./modules/tickets/tickets.module";
 import { BreedTransactionModule } from "./modules/breed-transaction/breed-transaction.module";
+import { PetOwnerModule } from "./modules/pet-owner/pet-owner.module";
 
 const mongoConnectionString = configService.getMongoConnectionString();
 @Module({
@@ -68,6 +69,7 @@ const mongoConnectionString = configService.getMongoConnectionString();
     ReportsModule,
     TicketsModule,
     BreedTransactionModule,
+    PetOwnerModule,
     CacheModule.register({
       isGlobal: true,
       ttl: 600,
--- src/entities/pet_service/pet-owner.entity.ts
+++ src/entities/pet_service/pet-owner.entity.ts
@@ -8,9 +8,10 @@ import {
 } from "typeorm";
 import { Pet } from "./pet.entity";
 import { Customer } from "../user_management_service/customer.entity";
+import { BaseEntity } from "typeorm";
 
 @Entity("pet_owner")
-export class PetOwner {
+export class PetOwner extends BaseEntity {
   @PrimaryGeneratedColumn("increment")
   id: number;
   @Column({ type: "bool", default: true })
@@ -30,4 +31,9 @@ export class PetOwner {
   @ManyToOne(() => Customer, (customer) => customer.petOwners, {})
   @JoinColumn({ name: "customerId", referencedColumnName: "id" })
   customer: Customer;
+
+  constructor(partial: Partial<PetOwner>) {
+    super();
+    Object.assign(this, partial);
+  }
 }
