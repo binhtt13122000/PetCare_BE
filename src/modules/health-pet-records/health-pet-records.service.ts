3926eaf6c887ced193e5804d71e57a35dbde91b5
Binh Truong <binhtt.13122000@gmail.com>
2022-06-14 23:28:26 +0700

    binhtt: add branch for api get list

--- src/modules/health-pet-records/health-pet-records.service.ts
+++ src/modules/health-pet-records/health-pet-records.service.ts
@@ -24,7 +24,7 @@ export class HealthPetRecordsService extends BaseService<
         petId: petId,
         type: type,
       },
-      relations: ["vaccine"],
+      relations: ["vaccine", "branch"],
     });
   }
 }
