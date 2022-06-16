1335f259d4e35e8d5c994349cc4606f6ebff7e0e
Binh Truong <binhtt.13122000@gmail.com>
2022-06-16 17:19:01 +0700

    binhtt: add chain

--- src/modules/pets/pets.module.ts
+++ src/modules/pets/pets.module.ts
@@ -4,9 +4,18 @@ import { PetsController } from "./pets.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PetsRepository } from "./pets.repository";
 import { SharedModule } from "src/shared/shared.module";
+import { HttpModule } from "@nestjs/axios";
+import { configService } from "src/config/config.service";
 
+const blockchainServer = configService.getBlockchainServer();
 @Module({
-  imports: [TypeOrmModule.forFeature([PetsRepository]), SharedModule],
+  imports: [
+    TypeOrmModule.forFeature([PetsRepository]),
+    SharedModule,
+    HttpModule.register({
+      baseURL: blockchainServer,
+    }),
+  ],
   controllers: [PetsController],
   providers: [PetsService],
   exports: [PetsService],
