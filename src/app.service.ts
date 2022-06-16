91154cd644e9c31ae10d25705b8d6c49703d38fc
Danh Van Pham <Danhskipper18@gmail.com>
2022-06-17 02:37:18 +0700

    [DanhPV] change cron patteren test

--- src/app.service.ts
+++ src/app.service.ts
@@ -21,7 +21,7 @@ export class AppService {
     this.logger.debug("Called when the current second is 45");
   }
 
-  @Cron("0 45 2 * * *", {
+  @Cron("0 0 3 * * *", {
     name: "checkExpiredTicket",
     timeZone: "Asia/Ho_Chi_Minh",
   })
